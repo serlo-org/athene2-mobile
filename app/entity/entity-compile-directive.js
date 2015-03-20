0b810aa703d9f83d8620ec7e215b269c3cf4039e
Jonas Keinholz <jonas@keinholz.com>
2015-03-20 14:06:08 +0100

    Sets BaseURI for images to API_HOST

--- app/entity/entity-compile-directive.js
+++ app/entity/entity-compile-directive.js
@@ -1,6 +1,17 @@
 'use strict';
-angular.module('entity.compileDirective', [])
-  .directive('compile', ['$compile', function ($compile) {
+angular.module('entity.compileDirective', ['config'])
+  .directive('compile', ['$compile', 'API_HOST', function ($compile, API_HOST) {
+    var replaceBaseUriInImages = function(element) {
+      if (element[0].tagName === 'IMG') {
+        element[0].src = element[0].src.replace(element[0].baseURI, API_HOST);
+      }
+
+      angular.forEach(element.children(), function(child) {
+        var element = angular.element(child);
+        replaceBaseUriInImages(element);
+      });
+    };
+
     return function(scope, element, attrs) {
       scope.$watch(
         function(scope) {
@@ -9,6 +20,9 @@ angular.module('entity.compileDirective', [])
         function(value) {
           element.html(value);
           $compile(element.contents())(scope);
+
+          replaceBaseUriInImages(element);
+
           MathJax.Hub.Queue(['Typeset', MathJax.Hub, element[0]]);
         }
       );
